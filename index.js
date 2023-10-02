67573db1c72af98f80ee070263eaae67c32c9280
unknown <trunghieu20032502@gmail.com>
2023-10-02 14:04:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/home', (req, res) => {
+    let a = 4;
+    let b = 5;
+    let c = a + b;
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
